20aba3ae54e80041c5f157084244975da6676579
Matt <m.zeller1901@gmail.com>
2020-04-16 09:52:46 +0200

    correct preprocessing for matlab
    the reading of the file parameters.txt was wrong in matlab (readcsv works differently)

--- Pre-processing/setup_preprocessing.m
+++ Pre-processing/setup_preprocessing.m
@@ -12,9 +12,9 @@ dt = 0.04;
 % WARNING: do not forget the / at the end !
 
 % Output folder, i.e. containing .mat files
-pathmats = '/media/maousi/Data/tmp/OM_Simulation_T30_N2000_2020_03_25/outputs/';
+pathmats = '/media/maousi/Data/tmp/simulation_noLVAD_2020_04_15/outputs/';
 %pathmats = '/media/maousi/Data/tmp/OM_Simulation_2020_03_20/outputs/';
 
 % Folder containing .mo files
-pathparams = '/media/maousi/Data/tmp/OM_Simulation_T30_N2000_2020_03_25/parameters.txt';
+pathparams = '/media/maousi/Data/tmp/simulation_noLVAD_2020_04_15/parameters.txt';
 %pathmos = '/Volumes/BONNEMAIN/2019_12_24/models/';
