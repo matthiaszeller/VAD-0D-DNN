fb893534a8c393e3af3e20aaae387e04de71bf15
Matt <m.zeller1901@gmail.com>
2020-04-22 00:46:51 +0200

    rpm 5000 preprocessing + DNN test

--- Pre-processing/setup_preprocessing.m
+++ Pre-processing/setup_preprocessing.m
@@ -12,9 +12,9 @@ dt = 0.04;
 % WARNING: do not forget the / at the end !
 
 % Output folder, i.e. containing .mat files
-pathmats = '/media/maousi/Data/tmp/simulation_noLVAD_2020_04_15/outputs/';
+pathmats = '/media/maousi/Data/tmp/simulation_LVAD_RPM5000_2020_04_21/outputs/';
 %pathmats = '/media/maousi/Data/tmp/OM_Simulation_2020_03_20/outputs/';
 
 % Folder containing .mo files
-pathparams = '/media/maousi/Data/tmp/simulation_noLVAD_2020_04_15/parameters.txt';
+pathparams = '/media/maousi/Data/tmp/simulation_LVAD_RPM5000_2020_04_21/parameters.txt';
 %pathmos = '/Volumes/BONNEMAIN/2019_12_24/models/';
--- Simulation_script/setup.py
+++ Simulation_script/setup.py
@@ -26,7 +26,8 @@ def q(msg):
 # ============ DNN TESTING
 
 # Location of the DNN training files
-dnn_folder = "/media/maousi/Data/tmp/simulation_noLVAD_2020_04_15/dnn"
+dnn_folder = "/media/maousi/Data/tmp/simulation_LVAD_RPM5000_2020_04_21/dnn"
 
 # Output folder path for the DNN testing
-output_folder_DNN_test = "/media/maousi/Data/tmp/simulation_noLVAD_2020_04_15/dnn_test"
+output_folder_DNN_test = "/media/maousi/Data/tmp/simulation_LVAD_RPM5000_2020_04_21/dnn_test"
+
