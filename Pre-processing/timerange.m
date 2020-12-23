a3b6eaff938894c403c1eaa0db67e0740daa9f43
matthiaszeller <44340346+maousi@users.noreply.github.com>
2020-12-23 14:13:26 +0100

    clean Preprocessing
    remove all matlab files, remove model files

--- Pre-processing/timerange.m
+++
@@ -1,8 +0,0 @@
-function [variableRange, time] = timerange(variable,t,t_min,t_max)
-% This function extract the variable (Variable), for a given interval of
-% time (t_min, t_max). It returns the value of the variable for this
-% interval of time.
-indices = find(t >= t_min & t <= t_max);
-variableRange=variable(indices);
-time=t(indices);
-end
