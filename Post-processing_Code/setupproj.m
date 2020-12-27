ca96edf205c7b836b1ad5737739db4ae443e3f0f
Matt <m.zeller1901@gmail.com>
2020-12-27 19:03:34 +0100

    fix post-processing and clean code

--- Post-processing_Code/setupproj.m
+++ Post-processing_Code/setupproj.m
@@ -1,19 +1,14 @@
 
 
-% test_resultcomparison.m
-% test_visualization.m
-% test_plotstageheartfailure.m
-% test_dnnmodelevaluation.m
+% 'outputs' folder of the DNN architecture (do not forget the '/' at the
+% end)
 output_path = '/media/maousi/Raw/lvad/dnns/dnn_5_layers_64_neurons_test/outputs/';
-%output_path = '/media/maousi/Data/tmp/test/';
-test_file_exact = [output_path, 'Ursino1998Model_VAD2_output_50_exact.mat'];
+
+% Any file in the output folder
+test_file_exact = [output_path, 'Ursino1998Model_VAD2_output_5_exact.mat'];
 
 
 % For sensitivity analysis
 %output_path = '/media/maousi/Raw/sensitivity/epsilon_0.2/P3_Param_LeftVentricle_kE/outputs/';
 %test_file_exact = [output_path, 'Ursino1998Model_VAD2_output_88.mat'];
-
-
-% Access scripts from Pre-processing/
-addpath('../Pre-processing/');
 
--- Post-processing_Code/test_dnnmodelevaluation.m
+++ Post-processing_Code/test_dnnmodelevaluation.m
@@ -2,13 +2,10 @@ clear
 
 % =========== PATH & FILE MANAGEMENT
 setupproj
+
 % Load the path where the data is. In this case, we want to load the results
 % of the 0-D simulations.
-%FilePathOutputs='/Users/jean.bonnemain/Documents/Code/0d_model/Deep_learning/2020_01_13/outputs/';
 FilePathOutputs = output_path; % from setupproj
-
-% We also want to load the parameters.
-FilePathParameters='/Users/jean.bonnemain/Documents/Code/0d_model/Deep_learning/2020_01_13/models/';
 
 filesExact = dir([FilePathOutputs,'*exact.mat']);
 filesPredicted = dir([FilePathOutputs,'*predicted.mat']);
@@ -30,28 +27,6 @@ TotalSimulationTime = (res.data_2(1,end));
 tsub_min=ceil((1/2)*TotalSimulationTime);
 tsub_max=TotalSimulationTime;
 
-% =========== DEFINE VARIABLES
-% Names of the variables we want to analyze. Names come from the 0D model.
-variablesname = {'SystemicArteries.PC','SAP'; ...
-                      'SystemicArteries.PC','SAP'; ...
-                      'PulmonaryArteries.PC', 'PAP'; ...
-                      'LeftVentricle.V', 'LVV'; ...
-                      'LeftVentricle.PV', 'LVP'; ...
-                      'RightVentricle.PV', 'RVP'; ...
-                      'RightVentricle.V', 'RVV'; ...
-                      'LeftAtrium.PC', 'LAP'; ...
-                      'AorticValve.Q', 'AoQ'; ...
-                      };
-                  
-
-% Values to extract and analyze :
-% LVEDV: Left ventricular end diastolic volume
-% LVESV: Left ventricular end systolic volume
-% LVEF: Left ventricular ejection fraction
-% Heart rate
-% SAPM, SAPS, SAPD
-% PAPM, PAPS, PAPD
-% Aortic flow, cardiac index
 
 [Xexact]=createdataforanalysis(filesExact,FilePathOutputs,tsub_min, tsub_max);
 [Xpredicted]=createdataforanalysis(filesPredicted,FilePathOutputs,tsub_min, tsub_max);
@@ -70,8 +45,6 @@ meanRelErrorMatrix=mean(abs(Xexact-Xpredicted)./(abs(Xexact)+1e-2));
 SDErrorMatrix=std(abs(Xexact-Xpredicted));
 [CImin,CImax]=confidenceinterval(abs(Xexact-Xpredicted),0.95);
 
-%[HR,SAPM,SAPS,SAPD,PAPM,PAPS,PAPD,LVEF, LVEDV, LVESV, CI]
-
 table = [
     MinExact;
     MaxExact;
@@ -87,10 +60,7 @@ table = [
     CImin;
     CImax;
 ];
+
 writematrix(table,'table.csv');
 writematrix(Xexact,'Xexact.csv');
 writematrix(Xpredicted,'Xpredicted.csv');
-
-%x=(abs(Xexact(:,7)-Xpredicted(:,7)));
-%pd=fitdist(x,'normal')
-%ci=paramci(pd)
